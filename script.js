4da6e48af04860bb48ff1e6751eed865fd3d83e2
Jackson <jacksondean.me@gmail.com>
2023-03-30 20:16:00 -0700

    Create base functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello');
+function getComputerChoice () {
+    let computerChoice = ['Rock', 'Paper', 'Scissors'];
+    return computerChoice[Math.floor(Math.random() * 3)].toLowerCase();
+}
+
+function getPlayerChoice () {
+    let playerChoice = prompt('Rock, paper, scissors, shoot!');
+    return playerChoice.toLowerCase();
+}
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+
+    return playerSelection + ' ' + computerSelection;
+}
+
+console.log(playRound());
+
+
+
