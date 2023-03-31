f50cab8185ff93ac46877a980b80761ba80a12ed
Jackson <jacksondean.me@gmail.com>
2023-03-31 13:24:39 -0700

    Add 5 round game functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function getComputerChoice () {
     let computerChoice = ['Rock', 'Paper', 'Scissors'];
-    return computerChoice[Math.floor(Math.random() * 3)].toLowerCase();
+    // return computerChoice[Math.floor(Math.random() * 3)].toLowerCase();
+    play = computerChoice[Math.floor(Math.random() * 3)].toLowerCase();
+    console.log('computer plays ' + play);
+    return play;
 }
 
 function getPlayerChoice () {
@@ -12,10 +15,44 @@ function playRound (playerSelection, computerSelection) {
     playerSelection = getPlayerChoice();
     computerSelection = getComputerChoice();
 
-    return playerSelection + ' ' + computerSelection;
+    if (playerSelection == computerSelection) {
+        return 0;
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 1;
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 1;
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 1;
+    } else if (computerSelection == 'rock' && computerSelection == 'scissors') {
+        return 2;
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+        return 2;
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+        return 2;
+    } else {
+        return 3;
+    }
 }
 
-console.log(playRound());
+function playGame () {
 
+    let playerScore = 0;
+    let computerScore = 0;
+    let i = 1;
 
+    while (i <= 5) {
+        let result = playRound();
+        console.log(result);
+        if (result == 1) {
+            playerScore++
+        } else if (result == 2) {
+            computerScore++
+        }
 
+        console.log('Round:' + i + ' Human:' + playerScore + ' Computer:' + computerScore);
+
+        i++
+    }
+}
+
+playGame();
